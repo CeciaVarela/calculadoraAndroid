f8bd38663c43a636641dd3f204fad378c1a39ccf
Cecía Varela <cvarelar@fpcoruna.afundacion.org>
2022-12-12 17:53:26 +0100

    3.findViewById creados

--- app/src/main/java/com/example/calculadoraandroid/MainActivity.java
+++ app/src/main/java/com/example/calculadoraandroid/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.calculadoraandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView Screen;
+    private Button AC,Power,Back,Div,Mul,Plus,Min,One,Two,Three,Four,Five,Six,Seven,Eight,Nine,Zero,Ans,Point,Equal;
+    private String input,Answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Screen = findViewById(R.id.screen);
+        AC = findViewById(R.id.ac);
+        Power = findViewById(R.id.power);
+        Back = findViewById(R.id.back);
+        Div = findViewById(R.id.div);
+        Seven = findViewById(R.id.seven);
+        Eight = findViewById(R.id.eight);
+        Nine = findViewById(R.id.nine);
+        Mul = findViewById(R.id.multiplication);
+        Four = findViewById(R.id.four);
+        Five = findViewById(R.id.five);
+        Six = findViewById(R.id.six);
+        Min = findViewById(R.id.min);
+        One = findViewById(R.id.one);
+        Two = findViewById(R.id.two);
+        Three = findViewById(R.id.three);
+        Plus = findViewById(R.id.plus);
+        Zero = findViewById(R.id.zero);
+        Point = findViewById(R.id.point);
+        Ans = findViewById(R.id.ans);
+        Equal = findViewById(R.id.equal);
     }
 }
